c99667f0ddbd323bda2d7e74050f88d72719246f
George  Pianka <gpianka1@gmail.com>
2019-05-01 13:04:39 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
 get '/admin', to: 'home#admin'
+root 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
